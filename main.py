3333b40aef533431fbea5d6576f52d51fb2ec264
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-20 16:24:45 -0700

    working with dictionaries

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
 push_ups = {
-    "push-ups": 0, 
-    "sets": 0,
-    "reps": 0,
-    "rest": 0,
+    "push-up": 2, 
+    "sets": 3,
+    "reps": 10,
+    "rest": 90, #seconds
     "notes": "Really focus on form!",
 }
+
+#print out each key in the dictionary
+keys = push_ups.keys()
+print(keys)
+
+#update the sets of the exercise
+push_ups["sets"] = 2
+
+#delete notes 
+del push_ups["notes"]
